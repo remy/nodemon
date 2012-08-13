4cc05ee77cb9a665b207742a864fb912c61f954f
Jason Griffin <jason.m.griffin@gmail.com>
2012-08-13 15:03:56 -0500

    handle cases where there is no child prcess

--- nodemon.js
+++ nodemon.js
@@ -364,8 +364,13 @@ function killNode() {
       // Force kill (/F) the whole child tree (/T) by PID (/PID 123)
       exec('taskkill /pid '+child.pid+' /T /F');
     } else {
-	  killedAfterChange = true;
-	  exec('pkill -P ' + child.pid );
+      killedAfterChange = true;
+  	  exec('pkill -P ' + child.pid, function (error, stdout, stderr) {
+  	    if (error !== null) {
+          killedAfterChange = false;
+  		    child.kill('SIGUSR2');
+  	    }
+  	  });
     }
   } else {
     startNode();
