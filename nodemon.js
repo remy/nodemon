ab6dc57e05ad39eac981d25007dd2cfcafab5960
Michael Lai <themichaellai@gmail.com>
2013-10-25 00:50:52 -0400

    Fix ignore rule regex

--- nodemon.js
+++ nodemon.js
@@ -773,7 +773,7 @@ if (program.options.delay) {
 
 // this is the default - why am I making it a cmd line opt?
 if (program.options.js) {
-  addIgnoreRule('^((?!\.js$|\.coffee|\.litcoffee$).)*$', true); // ignores everything except JS
+  addIgnoreRule('^((?!\.js$|\.coffee$|\.litcoffee$).)*$', true); // ignores everything except JS
 }
 
 if (program.options.watch && program.options.watch.length > 0) {
@@ -823,7 +823,7 @@ exists(ignoreFilePath, function (exist) {
           if (ext) {
             addIgnoreRule('^((?!' + ext + '$).)*$', true);
           } else {
-            addIgnoreRule('^((?!\.js$|\.coffee|\.litcoffee$).)*$', true); // ignores everything except JS
+            addIgnoreRule('^((?!\.js$|\.coffee$|\.litcoffee$).)*$', true); // ignores everything except JS
           }
         }
       }
