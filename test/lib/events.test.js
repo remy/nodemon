59e0135eb5ae82a7ee32a5c7d511e0cc15a33324
Remy Sharp <remy@remysharp.com>
2013-12-03 13:37:40 +0000

    More test off of lib

---
+++ test/lib/events.test.js
@@ -0,0 +1,20 @@
+/*global describe:true, it: true */
+var nodemon = require('../../lib/'),
+    assert = require('assert');
+
+describe('nodemon events', function () {
+  it('should have (shims) events', function () {
+    assert(nodemon.on);
+  });
+
+  it('should allow events to fire', function (done) {
+    nodemon.on('foo', function () {
+      assert(true);
+      done();
+    });
+
+    nodemon.emit('foo');
+  });
+});
+
+
---
+++ test/lib/require.test.js
@@ -0,0 +1,35 @@
+/*global describe:true, it: true */
+var nodemon = null,
+    assert = require('assert'),
+    path = require('path'),
+    touch = require('touch'),
+    appjs = path.resolve(__dirname, '..', 'fixtures', 'app.js');
+
+
+
+describe('require-able', function () {
+  beforeEach(function () {
+    nodemon = require('../../lib/');
+  });
+
+  it('should know nodemon has been required', function () {
+    console.log(nodemon.config);
+    assert(nodemon.config.required, 'nodemon has required property');
+  });
+
+  it('should restart on file change', function (done) {
+    var restarted = false;
+
+    nodemon(appjs).on('start', function () {
+      setTimeout(function () {
+        touch.sync(appjs);
+      }, 1000);
+    }).on('restart', function () {
+      restarted = true;
+      nodemon.emit('quit');
+    }).on('quit', function () {
+      assert(restarted);
+      done();
+    });
+  });
+});
