9673b849ac39e4c448229ee21724750ed1d6cef8
Remy Sharp <remy@remysharp.com>
2013-12-29 23:11:49 +0000

    Use update-notifier to keep nodemon up to date

--- bin/nodemon.js
+++ bin/nodemon.js
@@ -1,17 +1,15 @@
 #!/usr/bin/env node
-
+'use strict';
 var cli = require('../lib/cli'),
-    nodemon = require('../lib/');
-    // updateNotifier = require('update-notifier'),
+    nodemon = require('../lib/'),
+    updateNotifier = require('update-notifier'),
     // checks for available update and returns an instance
-    // notifier = updateNotifier({
-    //   packagePath: '../'
-    // });
+    notifier = updateNotifier({ packagePath: '../package' });
 
-// if (notifier.update) {
-//   // notify using the built-in convenience method
-//   notifier.notify();
-// }
+if (notifier.update) {
+  // notify using the built-in convenience method
+  notifier.notify();
+}
 
 var options = cli.parse(process.argv);
 options.restartable = 'rs';
