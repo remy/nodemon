a5cc35bfec2bfa26bd8e0b56fb4d25394960fab2
Remy Sharp <remy@remysharp.com>
2014-12-05 11:31:42 +0000

    remove logging in fixtures

--- test/fixtures/app.js
+++ test/fixtures/app.js
@@ -1,3 +1,3 @@
 // process.title = 'node (fixtures/app)';
-console.log('starting up @ ' + (process.env.PORT || 8000));
+// console.log('starting up @ ' + (process.env.PORT || 8000));
 require('http').createServer(function (req, res) { console.log('Request in'); res.end('ok'); }).listen(process.env.PORT || 8000);
