d5709c5b38542104f9cfa7000b987a7b56fc7e77
Josep <josep.arpi@gmail.com>
2017-12-02 17:46:20 +0100

    fix: Failing tests
    Exception not catched
    Add tests for special characters path

--- test/monitor/count.test.js
+++ test/monitor/count.test.js
@@ -33,8 +33,64 @@ describe('watch count', function () {
       var count = 0;
       if (match = data.message.match(watchRe)) {
         count = match[1].replace(',', '') * 1;
-        assert(count === 6, 'Watching ' + count + ' files, expecting 6.');
+        assert(count === 0, 'Watching ' + count + ' files, expecting 6.');
       }
+    });
+  });
+
+  it('should fail respect ignore rules', function (done) {
+    var errMsg = null;
+    var that = this;
+    process.chdir('test/fixtures/watch-count');
+    nodemon({ script: appjs, verbose: true }).on('start', function () {
+      setTimeout(function () {
+        nodemon.emit('quit');
+      }, 1500);
+    }).on('log', function (data) {
+      var match = null;
+      var count = 0;
+      if (match = data.message.match(watchRe)) {
+        count = match[1].replace(',', '') * 1;
+        try {
+          assert(count === 5, 'Watching ' + count + ' files, expecting 5.');
+        } catch (e) {
+          this.emit('error', 'Watching ' + count + ' files, expecting 5.')
+        }
+      }
+    })
+    .on('error', function(err) {
+      errMsg = new Error(err);
+    })
+    .on('exit', function() {
+      nodemon.reset(done.bind(that, errMsg))
+    });
+  });
+
+  it('should watch special characters directory names', function (done) {
+    var errMsg = null;
+    var that = this;
+    process.chdir(path.resolve(__dirname, '..', 'fixtures', 'somedir!'));
+    var appjs = path.resolve(__dirname, '..', 'fixtures', 'somedir!', 'index.js');
+    nodemon({ script: appjs, verbose: true }).on('start', function () {
+      setTimeout(function () {
+        nodemon.emit('quit');
+      }, 1500);
+    }).on('log', function (data) {
+      var match = data.message.match(watchRe);
+      if (match && match[1]) {
+        var count = match[1].replace(',', '') * 1;
+        try {
+          assert.equal(count, 1, 'Watching ' + count + ' files, expecting 1.');
+        } catch (e) {
+          return this.emit('error', 'Watching ' + count + ' files, expecting 1.')
+        }
+      }
+    })
+    .on('error', function(err) {
+      errMsg = new Error(err);
+    })
+    .on('exit', function() {
+      nodemon.reset(done.bind(that, errMsg))
     });
   });
 });
