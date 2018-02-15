83b8456064381a4919dc14d07d1bfcf4fa69d2ca
Remy Sharp <remy@remysharp.com>
2018-02-15 09:13:08 +0000

    fix: relative parent watching and ignore

--- test/monitor/watch-restart.test.js
+++ test/monitor/watch-restart.test.js
@@ -1,19 +1,21 @@
 'use strict';
 /*global describe, it, after, afterEach */
-var nodemon = require('../../lib/'),
-    assert = require('assert'),
-    fs = require('fs'),
-    utils = require('../utils'),
-    path = require('path'),
-    touch = require('touch'),
-    crypto = require('crypto'),
-    baseFilename = 'test/fixtures/test' + crypto.randomBytes(16).toString('hex');
+let debugLogger = {};
+const nodemon = require('../../lib/');
+var assert = require('assert');
+var fs = require('fs');
+var utils = require('../utils');
+var path = require('path');
+var touch = require('touch');
+var crypto = require('crypto');
+var baseFilename =
+  'test/fixtures/test' + crypto.randomBytes(16).toString('hex');
 
 var WAIT_BEFORE_START = 3000;
 
 describe('nodemon monitor child restart', function () {
-  var tmpjs = path.resolve(baseFilename + '.js'),
-      tmpmd = path.resolve(baseFilename + '.md');
+  var tmpjs = path.resolve(baseFilename + '.js');
+  var tmpmd = path.resolve(baseFilename + '.md');
 
   function write(both) {
     fs.writeFileSync(tmpjs, 'true;');
@@ -22,10 +24,11 @@ describe('nodemon monitor child restart', function () {
     }
   }
 
-  var pwd = process.cwd(),
-      oldhome = utils.home;
+  var pwd = process.cwd();
+  var oldhome = utils.home;
 
   afterEach(function () {
+    debugLogger = {};
     process.chdir(pwd);
     utils.home = oldhome;
 
@@ -38,51 +41,69 @@ describe('nodemon monitor child restart', function () {
   });
 
   after(function (done) {
-    nodemon.once('exit', function () {
-      nodemon.reset(done);
-    }).emit('quit');
+    nodemon
+      .once('exit', function () {
+        nodemon.reset(done);
+      })
+      .emit('quit');
   });
 
   it('should happen when monitoring a single extension', function (done) {
     write();
 
     setTimeout(function () {
-      nodemon({ script: tmpjs, verbose: true, ext: 'js' }).on('start', function () {
-        setTimeout(function () {
-          touch.sync(tmpjs);
-        }, 1500);
-      }).on('restart', function (files) {
-        assert(files[0] === tmpjs, 'nodemon restarted because of change to our file' + files);
-        nodemon.once('exit', function () {
-          nodemon.reset(done);
-        }).emit('quit');
-      });
+      nodemon({ script: tmpjs, verbose: true, ext: 'js' })
+        .on('start', function () {
+          setTimeout(function () {
+            touch.sync(tmpjs);
+          }, 1500);
+        })
+        .on('restart', function (files) {
+          assert(
+            files[0] === tmpjs,
+            'nodemon restarted because of change to our file' + files
+          );
+          nodemon
+            .once('exit', function () {
+              nodemon.reset(done);
+            })
+            .emit('quit');
+        });
     }, WAIT_BEFORE_START);
   });
 
   it('should happen when monitoring multiple extensions', function (done) {
     write(true);
     setTimeout(function () {
-
       nodemon({
         script: tmpjs,
         ext: 'js md',
-        verbose: true
-      }).on('start', function () {
-        setTimeout(function () {
-          touch.sync(tmpmd);
-        }, 1500);
-      }).on('log', function (event) {
-        var msg = event.message;
-        if (utils.match(msg, 'changes after filters')) {
-          var changes = msg.trim().slice(-5).split('/');
-          var restartedOn = changes.pop();
-          assert(restartedOn === '1', 'nodemon restarted on a single file change');
-          nodemon.once('exit', function () {
-            nodemon.reset(done);
-          }).emit('quit');
-        }
-      });
+        verbose: true,
+      })
+        .on('start', function () {
+          setTimeout(function () {
+            touch.sync(tmpmd);
+          }, 1500);
+        })
+        .on('log', function (event) {
+          var msg = event.message;
+          if (utils.match(msg, 'changes after filters')) {
+            var changes = msg
+              .trim()
+              .slice(-5)
+              .split('/');
+            var restartedOn = changes.pop();
+            assert(
+              restartedOn === '1',
+              'nodemon restarted on a single file change'
+            );
+            nodemon
+              .once('exit', function () {
+                nodemon.reset(done);
+              })
+              .emit('quit');
+          }
+        });
     }, WAIT_BEFORE_START);
   });
 
@@ -97,42 +118,83 @@ describe('nodemon monitor child restart', function () {
           script: tmpjs,
           verbose: true,
           ext: 'js',
-          watch: ['*.js', 'global']
-        }).on('start', function () {
-          setTimeout(function () {
-            touch.sync(tmpjs);
-          }, 1000);
-        }).on('restart', function (files) {
-          assert(files.length === 1, 'nodemon restarted when watching directory');
-          nodemon.once('exit', function () {
-            nodemon.reset(done);
-          }).emit('quit');
-        });
+          watch: ['*.js', 'global'],
+        })
+          .on('start', function () {
+            setTimeout(function () {
+              touch.sync(tmpjs);
+            }, 1000);
+          })
+          .on('restart', function (files) {
+            assert(
+              files.length === 1,
+              'nodemon restarted when watching directory'
+            );
+            nodemon
+              .once('exit', function () {
+                nodemon.reset(done);
+              })
+              .emit('quit');
+          });
       }, WAIT_BEFORE_START);
     });
   }
 
-
   it('should restart when watching directory', function (done) {
     write(true);
+
+    // process.chdir(process.cwd() + '/test/fixtures');
 
     setTimeout(function () {
       nodemon({
         script: tmpjs,
         verbose: true,
         ext: 'js md',
-        watch: ['test/fixtures/']
-      }).on('start', function () {
-        setTimeout(function () {
-          touch.sync(tmpmd);
-        }, 1000);
-      }).on('restart', function (files) {
-        assert(files.length === 1, 'nodemon restarted when watching directory');
-        nodemon.once('exit', function () {
-          nodemon.reset(done);
-        }).emit('quit');
-      });
+        watch: ['test/'],
+      })
+        .on('start', function () {
+          setTimeout(function () {
+            touch.sync(tmpmd);
+          }, 1000);
+        })
+        .on('restart', function (files) {
+          assert(
+            files.length === 1,
+            'nodemon restarted when watching directory'
+          );
+          nodemon
+            .once('exit', function () {
+              nodemon.reset(done);
+            })
+            .emit('quit');
+        });
     }, WAIT_BEFORE_START);
   });
 
+  it('should ignore relative node_modules', done => {
+    write(true);
+
+    process.chdir(process.cwd() + '/test/fixtures/1246/app');
+
+    nodemon({
+      script: 'index.js',
+      watch: ['../'],
+    })
+      .on('watching', file => {
+        assert(
+          file.indexOf('/node_modules/') === -1,
+          `node_modules found: ${file}`
+        );
+      })
+      .on('start', () => {
+        // gentle timeout to wait for the files to finish reading
+        setTimeout(() => {
+          nodemon
+            .once('exit', function () {
+              nodemon.reset(done);
+            })
+            .emit('quit');
+        }, 1000);
+      });
+  });
 });
