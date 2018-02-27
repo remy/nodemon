0f39b2e34936599b5ddb83c4450e5cbf4f8eb9f9
Remy Sharp <remy@remysharp.com>
2018-02-27 16:43:09 +0000

    fix: make it possible for windows to checkout (#1270)
    Fixes #1255

--- test/fork/run.test.js
+++ test/fork/run.test.js
@@ -1,8 +1,8 @@
 /*global describe:true, it: true */
 var assert = require('assert'),
-    utils = require('../utils'),
-    appjs = utils.appjs,
-    run = utils.run;
+  utils = require('../utils'),
+  appjs = utils.appjs,
+  run = utils.run;
 
 describe('nodemon fork', function () {
   it('should start a fork', function (done) {
@@ -30,77 +30,17 @@ describe('nodemon fork', function () {
         // make nodemon verbose so we can check the filters being applied
         args: ['-q', '--exec', 'test/fixtures/app\\ with\\ spaces.js']
       }, {
-        error: function (data) {
-          p.send('quit');
-          done(new Error(data));
-        },
-        output: function (data) {
-          // process.stdout.write(data);
-          if (data.trim() === 'OK') {
-            found = true;
-          }
-        }
-      });
-
-      p.on('message', function (event) {
-        if (event.type === 'start') {
-          setTimeout(function () {
+          error: function (data) {
             p.send('quit');
-            done();
-            assert(found, '"OK" message was found');
-          }, 500);
-        }
-      });
-    });
-
-    it('should start a fork exec with quotes and escaping', function (done) {
-      var found = false;
-      var p = run({
-        exec: 'bin/nodemon.js',
-        // make nodemon verbose so we can check the filters being applied
-        args: ['-q', '--exec', 'test/fixtures/some\\\"file']
-      }, {
-        error: function (data) {
-          p.send('quit');
-          done(new Error(data));
-        },
-        output: function (data) {
-          // process.stdout.write(data);
-          if (data.trim() === 'OK') {
-            found = true;
+            done(new Error(data));
+          },
+          output: function (data) {
+            // process.stdout.write(data);
+            if (data.trim() === 'OK') {
+              found = true;
+            }
           }
-        }
-      });
-
-      p.on('message', function (event) {
-        if (event.type === 'start') {
-          setTimeout(function () {
-            p.send('quit');
-            done();
-            assert(found, '"OK" message was found');
-          }, 500);
-        }
-      });
-    });
-
-    it('should start a fork exec with spaces and slashes', function (done) {
-      var found = false;
-      var p = run({
-        exec: 'bin/nodemon.js',
-        // make nodemon verbose so we can check the filters being applied
-        args: ['-q', '--exec', '"test/fixtures/some\ \\file"']
-      }, {
-        error: function (data) {
-          p.send('quit');
-          done(new Error(data));
-        },
-        output: function (data) {
-          // process.stdout.write(data);
-          if (data.trim() === 'OK') {
-            found = true;
-          }
-        }
-      });
+        });
 
       p.on('message', function (event) {
         if (event.type === 'start') {
@@ -120,16 +60,16 @@ describe('nodemon fork', function () {
         // make nodemon verbose so we can check the filters being applied
         args: ['-q', '--exec', '"test/fixtures/app with spaces.js" foo'],
       }, {
-        error: function (data) {
-          p.send('quit');
-          done(new Error(data));
-        },
-        output: function (data) {
-          if (data.trim() === 'foo') {
-            found = true;
+          error: function (data) {
+            p.send('quit');
+            done(new Error(data));
+          },
+          output: function (data) {
+            if (data.trim() === 'foo') {
+              found = true;
+            }
           }
-        }
-      });
+        });
 
       p.on('message', function (event) {
         if (event.type === 'start') {
@@ -151,17 +91,17 @@ describe('nodemon fork', function () {
       // make nodemon verbose so we can check the filters being applied
       args: ['-q', '--exec', 'test/fixtures/app\\ with\\ spaces.js foo']
     }, {
-      error: function (data) {
-        p.send('quit');
-        done(new Error(data));
-      },
-      output: function (data) {
-        // process.stdout.write(data);
-        if (data.trim() === 'foo') {
-          found = true;
+        error: function (data) {
+          p.send('quit');
+          done(new Error(data));
+        },
+        output: function (data) {
+          // process.stdout.write(data);
+          if (data.trim() === 'foo') {
+            found = true;
+          }
         }
-      }
-    });
+      });
 
     p.on('message', function (event) {
       if (event.type === 'start') {
