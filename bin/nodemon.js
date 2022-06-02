47e8d1c35cff1844b1c439e593e2661ac49e191e
Jimmy Wärting <jimmy@warting.se>
2022-06-02 15:36:58 +0000

    fix: remove update notifier

--- bin/nodemon.js
+++ bin/nodemon.js
@@ -5,12 +5,3 @@ const nodemon = require('../lib/');
 const options = cli.parse(process.argv);
 
 nodemon(options);
-
-const fs = require('fs');
-
-// checks for available update and returns an instance
-const pkg = JSON.parse(fs.readFileSync(__dirname + '/../package.json'));
-
-if (pkg.version.indexOf('0.0.0') !== 0 && options.noUpdateNotifier !== true) {
-  require('update-notifier')({ pkg }).notify();
-}
